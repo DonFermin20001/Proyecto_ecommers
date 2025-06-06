7a08f2538ef5e77f00f21f85662cc013ecff5412
s3r1s <losr95327@gmail.com>
2025-06-06 09:38:21 -0500

    cambios en el index

--- back/conector.py
+++ back/conector.py
@@ -1,4 +1,1 @@
-from supabase import create_client,Client
-from dotenv import load_dotenv
-
 print("hola mundo")
--- back/pagina.js
+++ back/pagina.js
@@ -1,1 +1,22 @@
-alert ("hola mundo")
+// Alquiler
+const brandFilter = document.getElementById("brandFilter");
+  const typeFilter = document.getElementById("typeFilter");
+  const tools = document.querySelectorAll(".tool-card");
+
+  function filterTools() {
+    const brand = brandFilter.value;
+    const type = typeFilter.value;
+
+    tools.forEach(tool => {
+      const matchBrand = brand === "all" || tool.classList.contains(brand);
+      const matchType = type === "all" || tool.classList.contains(type);
+      tool.classList.toggle("active", matchBrand && matchType);
+    });
+  }
+
+  brandFilter.addEventListener("change", filterTools);
+  typeFilter.addEventListener("change", filterTools);
+
+  // Mostrar todos por defecto
+  filterTools();
+  
