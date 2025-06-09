8819be7bfc0823f719f15ed75910d0438f90c277
DonFermin20001 <masterchecho18@gmail.com>
2025-06-09 10:30:20 -0500

    cambios

--- back/pagina.js
+++ back/pagina.js
@@ -2,19 +2,22 @@
   document.addEventListener('DOMContentLoaded', function () {
     const brandFilter = document.getElementById('brandFilter');
     const typeFilter = document.getElementById('typeFilter');
+    const stateFilter = document.getElementById('stateFilter')
     const toolCards = document.querySelectorAll('.tool-card');
 
     function filterTools() {
       const selectedBrand = brandFilter.value.toLowerCase();
       const selectedType = typeFilter.value.toLowerCase();
+      const selectedState = stateFilter.value.toLowerCase();
 
       toolCards.forEach(card => {
         const cardClasses = card.className.toLowerCase();
 
         const matchBrand = selectedBrand === 'all' || cardClasses.includes(selectedBrand);
         const matchType = selectedType === 'all' || cardClasses.includes(selectedType);
+        const matchState = selectedState === 'all' || cardClasses.includes(selectedState);
 
-        if (matchBrand && matchType) {
+        if (matchBrand && matchType && matchState) {
           card.style.display = '';
         } else {
           card.style.display = 'none';
@@ -24,7 +27,7 @@
 
     brandFilter.addEventListener('change', filterTools);
     typeFilter.addEventListener('change', filterTools);
-
+    stateFilter.addEventListener('change', filterTools);
     // Inicializar con todos visibles
     filterTools();
   });
@@ -50,10 +53,8 @@ function listcart() {
 
   let listado = "Productos en el carrito:\n\n";
   carrito.forEach((item, index) => {
-    listado += `${index + 1}. ${item.nombre} - $${item.valor.toFixed(2)}\n`;
+    listado += ${index + 1}. ${item.nombre} - $${item.valor.toFixed(2)}\n;
   });
 
   alert(listado); 
 }
-
-
